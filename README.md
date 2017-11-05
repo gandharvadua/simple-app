639efff2542005ebeae5cc3dfda605cd327bd762
gandharvadua <gandharvdua@gmail.com>
2017-11-05 19:09:00 +0530

    changes done

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# simple-app
-simple application using firebase
+
---
+++ src/environments/environment.ts
@@ -0,0 +1,16 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyC1RlW7Tqinrwlynn5Zs1uN_XgZqsZvzKA",
+    authDomain: "sample-e52a3.firebaseapp.com",
+    databaseURL: "https://sample-e52a3.firebaseio.com",
+    projectId: "sample-e52a3",
+    storageBucket: "sample-e52a3.appspot.com",
+    messagingSenderId: "121537323151"
+  }
+};
